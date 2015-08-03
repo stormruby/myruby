eaaec6e2765713df81ffd39978d53988ea34a11c
Michael Angerman <stormasm@gmail.com>
2015-08-03 16:01:46 -0700

    update pair

--- pair.rb
+++ pair.rb
@@ -17,33 +17,6 @@ def _get_random_pair
   values[1]
 end
 
-=begin
-def _get_random_pair
-  arr = [hpk,hpkto]
-  print arr; puts
-  while _test_random_pair
-  if _test_random_pair(arr)
-    puts 'got a good _test_random_pair'
-    arr
-  else
-    puts 'got a nil _test_random_pair'
-    nil
-  end
-end
-=end
-#values = _test_random_pair
-#print values[0], values[1]; puts
-
-#print _get_random_pair; puts
-
-=begin
-mypair = []
-while (mypair = _get_random_pair == nil)
-  print mypair; puts
-end
-print mypair; puts
-=end
-
 for i in (1..100)
   print _get_random_pair; puts
 end
