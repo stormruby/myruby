7cfacf3d1179734b8688f3479c7c0aeb7831351e
Michael Angerman <stormasm@gmail.com>
2015-09-30 19:08:13 -0700

    retry is working

--- retry01.rb
+++ retry01.rb
@@ -1,17 +1,22 @@
 
-def retry01
+def retry01(test1,test2)
   count = 0
   begin
-    if count > 15
-      exit
+    if count > test1
+      puts 'returning bye bye...'
+      return
     else
-      fail 'count is not 15'
+      print 'count is not ', test1; puts
+      fail
     end
   rescue
     count += 1
-    puts count
-    retry if count < 15
+    retry if count < test2
+      puts 'things did not work send email'
   end
 end
 
-retry01
+### return before sending email
+retry01(3,6)
+### email before returning as retry only happens 3 times
+retry01(6,3)
