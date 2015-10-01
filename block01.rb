fc43d216627fd1f7ea85ba7bb4cddf32729f1624
Michael Angerman <stormasm@gmail.com>
2015-09-30 18:01:28 -0700

    update block01

--- block01.rb
+++ block01.rb
@@ -3,3 +3,14 @@ def say_hi_to(&block)
 end
 
 say_hi_to { "michael" }
+
+def call_block
+  puts 'Start of method'
+  # you can call the block using the yield keyword
+  yield
+  yield
+  puts 'End of method'
+end
+
+# Code blocks may appear only in the source adjacent to a method call
+call_block {puts 'In the block'}
